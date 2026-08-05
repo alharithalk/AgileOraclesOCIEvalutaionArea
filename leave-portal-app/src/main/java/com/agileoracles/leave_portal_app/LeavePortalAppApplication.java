d90c948ab8d9c921b18f14bd978ab6484c9c5732
alharithalk <kindaking99@gmail.com>
2026-08-05 10:33:37 +0400

    Saved the evaluation project

--- leave-portal-app/src/main/java/com/agileoracles/leave_portal_app/LeavePortalAppApplication.java
+++ leave-portal-app/src/main/java/com/agileoracles/leave_portal_app/LeavePortalAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LeavePortalAppApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LeavePortalAppApplication.class, args);
 	}
