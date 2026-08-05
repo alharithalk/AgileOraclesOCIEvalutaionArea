b93c26bb441016e3dd2fb3e864c1994a8aed501e
alharithalk <kindaking99@gmail.com>
2026-08-05 15:41:31 +0400

    added leaveCategory.java

---
+++ leave-portal-app/leave-portal-app/src/main/java/com/agileoracles/leave_portal_app/model/LeaveCategory.java
@@ -0,0 +1,10 @@
+package com.agileoracles.leave_portal_app.model;
+
+public enum LeaveCategory {
+    SICK_LEAVE,
+    ANNUAL_LEAVE,
+    EMERGENCY_LEAVE,
+    MATERNITY_LEAVE,
+    UNPAID_LEAVE,
+    OTHER
+}
