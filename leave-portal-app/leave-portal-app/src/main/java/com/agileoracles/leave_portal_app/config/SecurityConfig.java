35ddf6e31938c60cf3963912490b52cdf5b35395
alharithalk <kindaking99@gmail.com>
2026-08-05 12:14:12 +0400

    added configurstion

---
+++ leave-portal-app/leave-portal-app/src/main/java/com/agileoracles/leave_portal_app/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.agileoracles.leave_portal_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/error", "/login/**", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/api/leave/status", true)
+                );
+        return http.build();
+    }
+}
