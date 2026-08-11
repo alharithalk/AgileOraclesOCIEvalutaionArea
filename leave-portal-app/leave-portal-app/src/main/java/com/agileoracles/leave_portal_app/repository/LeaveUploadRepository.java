4252fd81755e61e0b9e3e422f39ec65843baef44
alharithalk <kindaking99@gmail.com>
2026-08-11 14:28:46 +0400

    created  package com.agileoracles.leave_portal_app.repository;

---
+++ leave-portal-app/leave-portal-app/src/main/java/com/agileoracles/leave_portal_app/repository/LeaveUploadRepository.java
@@ -0,0 +1,13 @@
+package com.agileoracles.leave_portal_app.repository;
+
+
+
+import com.agileoracles.leave_portal_app.model.LeaveUploadRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface LeaveUploadRepository extends JpaRepository<LeaveUploadRecord, Long> {
+    List<LeaveUploadRecord> findByUserEmail(String userEmail);
+}
